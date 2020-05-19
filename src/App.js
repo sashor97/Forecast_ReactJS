c01f8e8e434977abd52a40d0726cc1b52bd847b6
sashor97 <54769781+sashor97@users.noreply.github.com>
2020-05-19 22:49:46 +0200

    Table style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            items:[]
+        }
+    }
+    componentDidMount() {
+        debugger;
+        axios.get("http://localhost:8080/weather/aboveFixedTemp").then(res => {
+            // console.log(res);
+            this.setState({
+                items: res.data
+            })
+            console.log(this.state.items);
+        }).catch((err) => {
+            console.log("Error: ", err);
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="text-center m-3">Welcome to Macedonian forecast in next 16 days</h1>
+                <div className="container" style={{maxWidth: "1140px !important"}}>
+
+                    <table className="table table-hover table-bordered table-dark">
+                        <thead>
+                        <tr className="bg-danger active">
+                            <td colspan="5" className="">City name</td>
+                            <td colspan="5">Date</td>
+                            <td colspan="5">Max day temperature</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.items.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td colspan="5">{item.city_name}</td>
+                                        <td colspan="5"> {item.date}</td>
+                                        <td colspan="5">{item.maxTemp}</td>
+                                    </tr>)
+                            })
+                        }
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
